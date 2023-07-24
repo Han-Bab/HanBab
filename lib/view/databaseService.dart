2377156c4eac331dcb726814b18f4409b7f48ef2
LocKey <rudfhr97@naver.com>
2023-07-24 21:43:41 +0900

    [feat] database remoting

--- lib/view/databaseService.dart
+++ lib/view/databaseService.dart
@@ -1,9 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class DatabaseService {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final FirebaseStorage storage = FirebaseStorage.instance;
+
+  final CollectionReference userCollection =
+  FirebaseFirestore.instance.collection("users");
+  final CollectionReference groupCollection =
+  FirebaseFirestore.instance.collection("groups");
+
+  final uid = FirebaseAuth.instance.currentUser?.uid;
 
   Future<String> getImage(String name) async {
     if(name == "") return "start";
@@ -27,5 +35,26 @@ class DatabaseService {
     }
   }
 
+  // creating a group
+  Future createGroup(String userName, String id, String groupName) async {
+    DocumentReference groupDocumentReference = await groupCollection.add({
+      "groupName": groupName,
+      "admin": "${id}_$userName",
+      "members": [],
+      "groupId": "",
+      "recentMessage": "",
+      "recentMessageSender": "",
+    });
+    // update the members
+    await groupDocumentReference.update({
+      "members": FieldValue.arrayUnion(["${uid}_$userName"]),
+      "groupId": groupDocumentReference.id,
+    });
 
+    DocumentReference userDocumentReference = userCollection.doc(uid);
+    return await userDocumentReference.update({
+      "groups":
+      FieldValue.arrayUnion(["${groupDocumentReference.id}_$groupName"])
+    });
+  }
 }
