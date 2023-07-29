e65ea769901c3beea0ad9e9d1f5d55c9f59339a5
Hyeongi Kim <21900215@handong.ac.kr>
2023-07-29 17:58:08 +0900

    [feat] 주문내역기능 및 UI 구현

---
+++ lib/model/order_info.dart
@@ -0,0 +1,20 @@
+class OrderInfo {
+  final String? groupId;
+  final String? groupName;
+  final String? pickup;
+  final String? currPeople;
+  final String? maxPeople;
+  final String? date;
+  final String? orderTime;
+  final String? imgUrl;
+
+  OrderInfo(
+      {this.groupId,
+      this.groupName,
+      this.pickup,
+      this.currPeople,
+      this.maxPeople,
+      this.date,
+      this.orderTime,
+      this.imgUrl});
+}
