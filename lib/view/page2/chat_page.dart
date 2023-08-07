303b041ef5192ef16e5629e9544bff2916ef19cf
LocKey <rudfhr97@naver.com>
2023-08-07 20:09:46 +0900

    [feat] 키보드 내리기

--- lib/view/page2/chat_page.dart
+++ lib/view/page2/chat_page.dart
@@ -35,6 +35,7 @@ class _ChatPageState extends State<ChatPage> {
   Stream<QuerySnapshot>? chats;
   TextEditingController messageController = TextEditingController();
   String admin = "";
+  FocusNode _focusNode = FocusNode();
 
   @override
   void initState() {
@@ -57,16 +58,19 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   Stream? members;
+
   getMembers() async {
-    DatabaseService()
-        .getGroupMembers(widget.groupId)
-        .then((val) {
+    DatabaseService().getGroupMembers(widget.groupId).then((val) {
       setState(() {
         members = val;
       });
     });
   }
-
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -75,145 +79,151 @@ class _ChatPageState extends State<ChatPage> {
         if (snapshot.hasData) {
           if (snapshot.data['members'] != null) {
             if (snapshot.data['members'].length != 0) {
-              return Scaffold(
-                appBar: AppBar(
-                  iconTheme: const IconThemeData(color: Colors.white),
-                  centerTitle: true,
-                  elevation: 0,
-                  title: Text(
-                    widget.groupName,
-                    style: const TextStyle(color: Colors.white),
+              return GestureDetector(
+                onTap: () {
+                  if (!_focusNode.hasFocus) {
+                    FocusScope.of(context).unfocus();
+                  }
+                },
+                child: Scaffold(
+                  appBar: AppBar(
+                    iconTheme: const IconThemeData(color: Colors.white),
+                    centerTitle: true,
+                    elevation: 0,
+                    title: Text(
+                      widget.groupName,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    leading: IconButton(
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => const App()),
+                            (route) => true);
+                      },
+                      icon: const Icon(Icons.arrow_back_ios_new),
+                    ),
                   ),
-                  backgroundColor: Theme.of(context).primaryColor,
-                  leading: IconButton(
-                    onPressed: () {
-                      Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => const App()),
-                          (route) => true);
-                    },
-                    icon: const Icon(Icons.arrow_back_ios_new),
+                  endDrawer: Drawer(
+                    child: EndDrawer(
+                        groupId: widget.groupId,
+                        groupName: widget.groupName,
+                        groupTime: widget.groupTime,
+                        groupPlace: widget.groupPlace,
+                        groupAll: widget.groupAll,
+                        admin: admin,
+                        userName: widget.userName,
+                        members: snapshot.data['members']),
                   ),
-                ),
-                endDrawer: Drawer(
-                  child: EndDrawer(
-                      groupId: widget.groupId,
-                      groupName: widget.groupName,
-                      groupTime: widget.groupTime,
-                      groupPlace: widget.groupPlace,
-                      groupAll: widget.groupAll,
-                      admin: admin,
-                    userName: widget.userName,
-                    members: snapshot.data['members']
-                  ),
-                ),
-                body: Column(
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                          border: Border(
-                        bottom:
-                            BorderSide(width: 1.0, color: Color(0xffE1E1E1)),
-                      )),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 13, 13, 13),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Image.asset(
-                              "./assets/icons/info.png",
-                              scale: 1.8,
-                            ),
-                            Row(
-                              children: [
-                                const Text("시간: "),
-                                Text(widget.groupTime),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text("장소: "),
-                                Text(widget.groupPlace),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text("인원: "),
-                                Text(
-                                    "${snapshot.data['members'].length}/${widget.groupAll}"),
-                              ],
-                            ),
+                  body: Column(
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                            border: Border(
+                          bottom:
+                              BorderSide(width: 1.0, color: Color(0xffE1E1E1)),
+                        )),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 13, 13, 13),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image.asset(
+                                "./assets/icons/info.png",
+                                scale: 1.8,
+                              ),
+                              Row(
+                                children: [
+                                  const Text("시간: "),
+                                  Text(widget.groupTime),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("장소: "),
+                                  Text(widget.groupPlace),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("인원: "),
+                                  Text(
+                                      "${snapshot.data['members'].length}/${widget.groupAll}"),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Stack(
+                          children: <Widget>[
+                            chatMessages(),
+                            Container(
+                              alignment: Alignment.bottomCenter,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 40),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: Color(0xFFffffff),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        color: Colors.grey,
+                                        blurRadius: 10.0, // soften the shadow
+                                      )
+                                    ],
+                                  ),
+                                  child: Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(24, 3, 8, 3),
+                                    child: Row(children: [
+                                      Expanded(
+                                          child: TextFormField(
+                                        controller: messageController,
+                                        style:
+                                            const TextStyle(color: Colors.black),
+                                        decoration: const InputDecoration(
+                                          hintText: "메시지 입력하세요",
+                                          hintStyle: TextStyle(
+                                              color: Color(0xff919191),
+                                              fontSize: 16),
+                                          //회색
+                                          border: InputBorder.none,
+                                        ),
+                                      )),
+                                      const SizedBox(
+                                        width: 12,
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          sendMessage();
+                                        },
+                                        child: Container(
+                                          height: 40,
+                                          width: 40,
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(30),
+                                          ),
+                                          child: Center(
+                                              child: Image.asset(
+                                                  "./assets/icons/message.png")),
+                                        ),
+                                      )
+                                    ]),
+                                  ),
+                                ),
+                              ),
+                            )
                           ],
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Stack(
-                        children: <Widget>[
-                          chatMessages(),
-                          Container(
-                            alignment: Alignment.bottomCenter,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 40),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  color: Color(0xFFffffff),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                      color: Colors.grey,
-                                      blurRadius: 10.0, // soften the shadow
-                                    )
-                                  ],
-                                ),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(24, 3, 8, 3),
-                                  child: Row(children: [
-                                    Expanded(
-                                        child: TextFormField(
-                                      controller: messageController,
-                                      style:
-                                          const TextStyle(color: Colors.black),
-                                      decoration: const InputDecoration(
-                                        hintText: "메시지 입력하세요",
-                                        hintStyle: TextStyle(
-                                            color: Color(0xff919191),
-                                            fontSize: 16),
-                                        //회색
-                                        border: InputBorder.none,
-                                      ),
-                                    )),
-                                    const SizedBox(
-                                      width: 12,
-                                    ),
-                                    GestureDetector(
-                                      onTap: () {
-                                        sendMessage();
-                                      },
-                                      child: Container(
-                                        height: 40,
-                                        width: 40,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius:
-                                              BorderRadius.circular(30),
-                                        ),
-                                        child: Center(
-                                            child: Image.asset(
-                                                "./assets/icons/message.png")),
-                                      ),
-                                    )
-                                  ]),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             } else {
@@ -249,8 +259,7 @@ class _ChatPageState extends State<ChatPage> {
                       sender: snapshot.data.docs[index]['sender'],
                       sentByMe: widget.userName ==
                           snapshot.data.docs[index]['sender'],
-                    isEnter: snapshot.data.docs[index]['isEnter']
-                  );
+                      isEnter: snapshot.data.docs[index]['isEnter']);
                 },
               )
             : Container();
