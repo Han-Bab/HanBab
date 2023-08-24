b2f90f8d3dbf365cdbf05ebe2c1535e6fa38a08c
yeon38 <chaeyeon6243@handong.ac.kr>
2023-08-24 11:23:38 +0900

    [feat] login design

--- lib/view/login/initial.dart
+++ lib/view/login/initial.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:han_bab/controller/auth_controller.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
 class InitialPage extends StatelessWidget {
@@ -17,51 +18,78 @@ class InitialPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+        padding: const EdgeInsets.symmetric(vertical: 20),
         child: Column(
           children: [
             /// 디자인을 위한 빈 공간 (디자인 해주세요)
             SizedBox(height: 5),
-            Image.asset('assets/images/orangeLogo.png'),
+            Image.asset(
+              'assets/images/logoOrange.png',
+              scale: 2,
+            ),
             SizedBox(height: 35),
             const Text(
-                 '함께 모여서 맛있게 먹는 행복한 식사',
-                 style: TextStyle(
-                     fontSize: 20,
-                     color: Colors.black,
-                   ),
-                 ),
+              '서로의 뜻을 모아 모두가 행복한 시간',
+              style: TextStyle(
+                fontSize: 20,
+                color: Color(0xff717171),
+              ),
+            ),
             const Text(
               '한동 밥먹자에서 시작하세요',
               style: TextStyle(
                 fontSize: 20,
-                color: Colors.black,
+                color: Color(0xff717171),
               ),
             ),
-            SizedBox(height: 40),
-            Image.asset('assets/images/dinner.png'),
-            SizedBox(height: 78),
 
+            Expanded(
+              child: Stack(
+                children: [
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                        child: Lottie.asset('assets/lottie/person.json'),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 35, left: 194),
+                    child: Container(
+                      color: Color(0xffFFFBFF),
+                      width: 140,
+                      height: 250,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 120, bottom: 90.0),
+                    child: Container(height: 300,child: Lottie.asset('assets/lottie/chatting.json')),
+                  )
+                ],
+              ),
+            ),
 
             /// 이메일 로그인 버튼
-            SizedBox(
-              width: screenWidth,
-              height: screenHeight * 0.065,
-              child: ElevatedButton(
-                onPressed: () {
-                  // authController.signInWithGoogle();
-                  Navigator.pushNamed(context, '/login');
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(10),
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-                child: const Text(
-                  '이메일로 로그인',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: SizedBox(
+                width: screenWidth,
+                height: screenHeight * 0.065,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // authController.signInWithGoogle();
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  child: const Text(
+                    '이메일로 로그인',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
@@ -100,7 +128,7 @@ class InitialPage extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(24.0),
+        padding: const EdgeInsets.only(bottom: 48.0),
         child: TextButton(
           onPressed: () {
             Navigator.pushNamed(context, '/signup1');
