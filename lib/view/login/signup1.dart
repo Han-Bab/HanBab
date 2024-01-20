30f40017196aea4538c70602ac00dc6624920501
LocKey <rudfhr97@naver.com>
2024-01-20 23:31:18 +0900

    [Modify] 회원가입화면 디자인 수정중

--- lib/view/login/signup1.dart
+++ lib/view/login/signup1.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_formatter/formatters/masked_input_formatter.dart';
 import 'package:han_bab/controller/signup_controller.dart';
 import 'package:provider/provider.dart';
 
+import '../../color_schemes.dart';
 import '../../widget/button.dart';
 
 class Signup1Page extends StatelessWidget {
@@ -17,7 +19,7 @@ class Signup1Page extends StatelessWidget {
       },
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('1 / 3'),
+          title: const Text('회원가입'),
           flexibleSpace: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
@@ -35,42 +37,42 @@ class Signup1Page extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                padding: const EdgeInsets.all(30),
+                padding: const EdgeInsets.all(24),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    RichText(
-                        text: const TextSpan(
-                      children: [
-                        TextSpan(
-                          text: '한밥',
-                          style: TextStyle(
-                            color: Colors.orange,
-                            fontSize: 23,
-                          ),
-                        ),
-                        TextSpan(
-                          text: '을 통해 행복한 식사에\n참여해보세요',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 116, 116, 116),
-                            fontSize: 23,
-                          ),
-                        ),
-                      ],
-                    )),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 8.0, left: 5),
-                      child: Text(
-                        "이메일 주소를 입력해주세요",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
+                    TextFormField(
+                      onChanged: (value) {
+                        controller.setName(value);
+                      },
+                      decoration: InputDecoration(
+                        errorText: controller.nameErrorText,
+                        hintText: "이름",
+                        hintStyle: const TextStyle(
+                            fontSize: 18, fontFamily: "PretendardLight"),
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(0, 10, 10, 10),
                       ),
                     ),
-                    // 한동 이메일 입력
+                    // TextFormField(
+                    //   keyboardType: TextInputType.emailAddress,
+                    //   onSaved: (value) {
+                    //     controller.setEmail(value!);
+                    //   },
+                    //   onChanged: (value) {
+                    //     controller.setEmail(value);
+                    //   },
+                    //   focusNode: controller.emailFocus,
+                    //   decoration: InputDecoration(
+                    //     errorText: controller.emailErrorText,
+                    //     hintText: "example@handong.ac.kr",
+                    //     contentPadding:
+                    //         const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                    //   ),
+                    // ),
+                    const SizedBox(
+                      height: 27,
+                    ),
                     TextFormField(
                       keyboardType: TextInputType.emailAddress,
                       onSaved: (value) {
@@ -82,59 +84,108 @@ class Signup1Page extends StatelessWidget {
                       focusNode: controller.emailFocus,
                       decoration: InputDecoration(
                         errorText: controller.emailErrorText,
-                        hintText: "example@handong.ac.kr",
+                        hintText: "이메일",
+                        hintStyle: const TextStyle(
+                            fontSize: 18, fontFamily: "PretendardLight"),
                         contentPadding:
-                            const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                            const EdgeInsets.fromLTRB(0, 10, 10, 10),
                       ),
                     ),
                     const SizedBox(
-                      height: 30,
+                      height: 27,
                     ),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 8.0, left: 5),
-                      child: Text(
-                        "비밀번호를 입력해주세요\n(8자 이상의 영문 + 숫자 + 특수문자 조합)",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            keyboardType: TextInputType.phone,
+                            onChanged: (value) {
+                              controller.setPhone(value);
+                            },
+                            decoration: InputDecoration(
+                              errorText: controller.phoneErrorText,
+                              hintText: "휴대폰 번호",
+                              hintStyle: const TextStyle(
+                                  fontSize: 18, fontFamily: "PretendardLight"),
+                              contentPadding:
+                                  const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                            ),
+                            inputFormatters: [
+                              MaskedInputFormatter("000-0000-0000")
+                            ],
+                          ),
                         ),
-                      ),
+                        const SizedBox(
+                          width: 18,
+                        ),
+                        GestureDetector(
+                          onTap: !controller.verified &&
+                                  controller.phone != "" &&
+                                  !controller.verifying
+                              ? () async {
+                                  await controller.verifyPhoneNumber(context);
+                                }
+                              : null,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: !controller.verified &&
+                                        controller.phone != "" &&
+                                        !controller.verifying
+                                    ? const Color(0xffFDB168)
+                                    : const Color(0xffC2C2C2)),
+                            child: const Padding(
+                              padding: EdgeInsets.fromLTRB(12, 7, 12, 7),
+                              child: Text(
+                                "인증요청",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontFamily: "PretendardMedium",
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
+                    const SizedBox(
+                      height: 27,
+                    ),
+                    controller.verifying
+                        ? controller.verifyCode(context)
+                        : Container(),
+
                     // 비밀번호 입력폼
-                    TextFormField(
-                      decoration: InputDecoration(
-                        errorText: controller.passwordErrorText,
-                        hintText: "비밀번호를 입력해주세요",
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(5, 15, 15, 15),
-                      ),
-                      obscureText: true,
-                      focusNode: controller.pwFocus,
-                      onSaved: (value) {
-                        controller.setPassword(value!);
-                      },
-                      onChanged: (value) {
-                        controller.setPassword(value);
-                      },
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        errorText: controller.passwordConfirmErrorText,
-                        hintText: "비밀번호를 한번 더 입력해주세요",
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(5, 15, 15, 15),
-                      ),
-                      focusNode: controller.pwConfirmFocus,
-                      obscureText: true,
-                      onChanged: (value) {
-                        controller.setPasswordConfirm(value);
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
+                    // TextFormField(
+                    //   decoration: InputDecoration(
+                    //     errorText: controller.passwordErrorText,
+                    //     hintText: "비밀번호를 입력해주세요",
+                    //     contentPadding:
+                    //         const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                    //   ),
+                    //   obscureText: true,
+                    //   focusNode: controller.pwFocus,
+                    //   onSaved: (value) {
+                    //     controller.setPassword(value!);
+                    //   },
+                    //   onChanged: (value) {
+                    //     controller.setPassword(value);
+                    //   },
+                    // ),
+
+                    // TextFormField(
+                    //   decoration: InputDecoration(
+                    //     errorText: controller.passwordConfirmErrorText,
+                    //     hintText: "비밀번호를 한번 더 입력해주세요",
+                    //     contentPadding:
+                    //         const EdgeInsets.fromLTRB(5, 15, 15, 15),
+                    //   ),
+                    //   focusNode: controller.pwConfirmFocus,
+                    //   obscureText: true,
+                    //   onChanged: (value) {
+                    //     controller.setPasswordConfirm(value);
+                    //   },
+                    // ),
                   ],
                 ),
               ),
@@ -144,15 +195,17 @@ class Signup1Page extends StatelessWidget {
         bottomNavigationBar: Padding(
           padding: const EdgeInsets.symmetric(vertical: 34, horizontal: 28),
           child: SizedBox(
-            height: 42,
+            height: 60,
             child: Button(
-              function: () {
-                if (controller.step1Validation()) {
-                  Navigator.pushNamed(
-                      context, '/signup2');
-                }
-              },
-              title: '다음',
+              function: controller.name == "" ||
+                      controller.email == "" ||
+                      controller.phone == "" ||
+                      controller.verified == false
+                  ? null
+                  : () {
+                      Navigator.pushNamed(context, '/signup2');
+                    },
+              title: '다음 단계로 이동하기',
             ),
           ),
         ),
