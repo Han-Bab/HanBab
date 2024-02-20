3abb30b0317f4faaf43eaa46903bad2a1ea67269
Hyeongi Kim <21900215@handong.ac.kr>
2024-02-20 17:23:50 +0900

    [Feat] 방만들기 기능 구현

--- lib/controller/map_provider.dart
+++ lib/controller/map_provider.dart
@@ -4,10 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class MapProvider extends ChangeNotifier {
-  final _searchList = ['현재 검색결과가 없습니다'];
-  get searchList => _searchList;
+  // final _searchList = ['현재 검색결과가 없습니다'];
+  // get searchList => _searchList;
 
   var json = {};
+  var restaurantInfo = {};
+
+  String restaurantName = '';
+  double latitude = 0;
+  double longitude = 0;
 
   ValueKey mapKey = ValueKey(DateTime.now());
 
@@ -16,11 +21,6 @@ class MapProvider extends ChangeNotifier {
 
     notifyListeners();
   }
-
-  Map<String, dynamic> selectedJson = {};
-  String selectedName = '';
-  double selectedLatitude = 0;
-  double selectedLongitude = 0;
 
   /* 가게 고유 ID */
   String _placeNameField = '';
@@ -47,25 +47,26 @@ class MapProvider extends ChangeNotifier {
     }
   }
 
-  void setSelectedJson(String selection) {
-    selectedJson =
-        json['documents'].firstWhere((d) => d['place_name'] == selection);
+  // void setSelectedJson(String selection) {
+  //   selectedJson =
+  //       json['documents'].firstWhere((d) => d['place_name'] == selection);
 
-    print(selectedJson);
+  //   print(selectedJson);
 
-    selectedName = selection;
-    selectedLatitude = double.parse(selectedJson['y']);
-    selectedLongitude = double.parse(selectedJson['x']);
+  //   selectedName = selection;
+  //   selectedLatitude = double.parse(selectedJson['y']);
+  //   selectedLongitude = double.parse(selectedJson['x']);
 
-    notifyListeners();
-  }
+  //   notifyListeners();
+  // }
 
   Future<void> kakaoLocalSearchKeyword(String keyword) async {
-    _searchList.clear();
+    // _searchList.clear();
+    restaurantInfo = {};
     try {
       http.Response response = await http.get(
         Uri.parse(
-            "https://dapi.kakao.com/v2/local/search/keyword.json?x=129.388849&y=36.103255&radius=10000&query=$keyword&size=5&category_group_code=FD6,CE7"),
+            "https://dapi.kakao.com/v2/local/search/keyword.json?x=129.388849&y=36.103255&radius=10000&query=$keyword&size=1&category_group_code=FD6,CE7"),
         headers: {
           // 임시입니다
           "Authorization": "KakaoAK 4d056234a214748603ae4c444eae0c5d",
@@ -73,9 +74,14 @@ class MapProvider extends ChangeNotifier {
       );
       if (response.statusCode == 200) {
         json = jsonDecode(response.body);
-        // print(json['documents']);
-        for (var item in json['documents']) {
-          _searchList.add(item['place_name']);
+        List<dynamic> doc = json['documents'];
+        if (doc.isEmpty) {
+          print("아쉽게도 위치정보를 제공하지 않는 가게입니다..");
+        } else {
+          restaurantInfo = doc[0];
+          restaurantName = keyword;
+          latitude = double.parse(restaurantInfo['y']);
+          longitude = double.parse(restaurantInfo['x']);
         }
       }
       notifyListeners();
@@ -85,14 +91,15 @@ class MapProvider extends ChangeNotifier {
   }
 
   void clearAll() {
-    _searchList.clear();
+    // _searchList.clear();
     json = {};
-    selectedJson = {};
-    selectedName = '';
+    restaurantInfo = {};
+    // selectedJson = {};
+    restaurantName = '';
     placeImageUrl = '';
     _placeNameField = '';
-    selectedLatitude = 0;
-    selectedLongitude = 0;
+    latitude = 0;
+    longitude = 0;
 
     notifyListeners();
   }
