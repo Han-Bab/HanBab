d34161df9604badd25cc033465ca7a20f3e68971
LocKey <rudfhr97@naver.com>
2024-11-03 00:34:45 +0900

    [Fix] 프로필 상세 정보 버튼 위치 수정

--- lib/view/page3/profileModify.dart
+++ lib/view/page3/profileModify.dart
@@ -51,100 +51,103 @@ class _ProfileModifyState extends State<ProfileModify> {
           children: [
             appbar(context, "프로필 상세정보"),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(24.0, 30, 24, 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("내 정보 변경"),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 3.0),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              const SizedBox(
-                                height: 24,
-                              ),
-                              TextFormField(
-                                enabled: false,
-                                controller: nameController,
-                                onChanged: (value) {
-                                  setState(() {
-                                    nameController.text = value;
-                                  });
-                                },
-                              ),
-                              const SizedBox(
-                                height: 24,
-                              ),
-                              TextFormField(
-                                enabled: false,
-                                controller: emailController,
-                                onChanged: (value) {
-                                  setState(() {
-                                    emailController.text = value;
-                                  });
-                                },
-                              ),
-                              const SizedBox(
-                                height: 24,
-                              ),
-                              TextFormField(
-                                enabled: false,
-                                controller: phoneController,
-                                onChanged: (value) {
-                                  setState(() {
-                                    phoneController.text = value;
-                                  });
-                                },
-                              ),
-                              const SizedBox(
-                                height: 24,
-                              ),
-                              TextFormField(
-                                controller: accountController,
-                                onChanged: (value) {
-                                  setState(() {
-                                    accountController.text = value;
-                                  });
-                                },
-                              ),
-                              const SizedBox(
-                                height: 24,
-                              ),
-                              const Text("개인 정보 수정시 고객센터로 문의 주시기 바랍니다.")
-                            ],
+              child: SafeArea(
+                top: false,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(24.0, 30, 24, 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("내 정보 변경"),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 3.0),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: 24,
+                                ),
+                                TextFormField(
+                                  enabled: false,
+                                  controller: nameController,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      nameController.text = value;
+                                    });
+                                  },
+                                ),
+                                const SizedBox(
+                                  height: 24,
+                                ),
+                                TextFormField(
+                                  enabled: false,
+                                  controller: emailController,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      emailController.text = value;
+                                    });
+                                  },
+                                ),
+                                const SizedBox(
+                                  height: 24,
+                                ),
+                                TextFormField(
+                                  enabled: false,
+                                  controller: phoneController,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      phoneController.text = value;
+                                    });
+                                  },
+                                ),
+                                const SizedBox(
+                                  height: 24,
+                                ),
+                                TextFormField(
+                                  controller: accountController,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      accountController.text = value;
+                                    });
+                                  },
+                                ),
+                                const SizedBox(
+                                  height: 24,
+                                ),
+                                const Text("개인 정보 수정시 고객센터로 문의 주시기 바랍니다.")
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        child: Button(
-                          backgroundColor: Theme.of(context).primaryColor,
-                          function: () async {
-                            FocusScope.of(context).unfocus();
-                            await DatabaseService().modifyUserInfo(
-                                nameController.text,
-                                emailController.text,
-                                phoneController.text,
-                                accountController.text);
-              
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(const SnackBar(
-                              content: Text('정보가 변경되었습니다.'),
-                              duration: Duration(seconds: 5),
-                            ));
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const App()),
-                                (route) => false);
-                          },
-                          title: '저장하기',
-                        ))
-                  ],
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Button(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            function: () async {
+                              FocusScope.of(context).unfocus();
+                              await DatabaseService().modifyUserInfo(
+                                  nameController.text,
+                                  emailController.text,
+                                  phoneController.text,
+                                  accountController.text);
+
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(const SnackBar(
+                                content: Text('정보가 변경되었습니다.'),
+                                duration: Duration(seconds: 5),
+                              ));
+                              Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const App()),
+                                  (route) => false);
+                            },
+                            title: '저장하기',
+                          ))
+                    ],
+                  ),
                 ),
               ),
             ),
